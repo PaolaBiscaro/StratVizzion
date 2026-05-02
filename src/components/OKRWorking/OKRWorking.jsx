0d0253ac8ec382ea945d491ed0c4fdfe202543b1
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-05-02 04:07:14 -0300

    Small fix

--- src/components/OKRWorking/OKRWorking.jsx
+++ src/components/OKRWorking/OKRWorking.jsx
@@ -14,15 +14,11 @@ function OKRCHart() {
     return (
         <div className="OKRChart">
             <h3 className="title-card">OKR - Objetivos e Resultados-Chave</h3>
-            <div className="okr-cards"> {/* 👈 envolve os cards */}
+            <div className="okr-cards"> 
                 <ArcChart value={86} title="OKR_0001" date="29/03/2027" description="Atingir R$ 500 mil em Novos Negócios até o final do trimestre."/>
                 <ArcChart value={20} title="OKR_0002" date="30/04/2027" description="Reduzir o custo por aquisição (CPA) de R$45 para R$28." />
                 <ArcChart value={1} title="OKR_0003" date="30/05/2027" description="Atingir taxa de conversão do onboarding de 40% para 62%." />
                 <ArcChart value={50} title="OKR_0004" date="29/03/2027" description="Atingir R$ 500 mil em Novos Negócios até o final do trimestre." />
-<<<<<<< HEAD
-
-=======
->>>>>>> b30ef7ff8735834dee5fe07461a5973962cac538
             </div>
         </div>
     );
