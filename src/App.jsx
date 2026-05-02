e89130488ca78e56a07c4be58aa64483cf331448
Joaquim_Oliveira <yunohanayuunasan@gmail.com>
2026-05-02 11:02:45 -0300

    feat: telas de login e cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
 import React from "react";
-import "./styles/variables.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NewOKR from "./pages/NewOKR";
-import NewKR from "./pages/NewKR";
-import SideBar from "./components/Sidebar/SideBar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/HomeDirector";
+import { Routes, Route } from "react-router-dom";
 
-
-
-/*Explicação
-  Usando a biblioteca React router eu consigo colocar várias páginas diferentes
-  que direcionam aos arquivos, assim ao rodar npm run dev é possível cada arquivo
-  da pasta pages ter seu próprio URL
-
-*/
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
 
 function App() {
   return (
-    <BrowserRouter>
     <Routes>
-      <Route path="/NewOKR" element={<NewOKR/>}/>
-      <Route path="/NewKR" element={<NewKR/>}/>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
     </Routes>
-
-    </BrowserRouter>
   );
 }
 
